9e2fbea7719b7c8dc491f75b8826fa3b5f5e8cef
Navraj singh kaler <navrajkaler996@gmail.com>
2025-12-02 18:59:04 -0600

    cors added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const cors = require("cors");
+
 const { AppDataSource } = require("./data-source");
 
 const roomRoutes = require("./routes/roomRoutes");
 
 const app = express();
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 
 app.use("/api/rooms", roomRoutes);
 
