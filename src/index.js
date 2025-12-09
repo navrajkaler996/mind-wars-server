c1c47c5d4116e4553be9a9073b228ab0abf3e95a
Navraj singh kaler <navrajkaler996@gmail.com>
2025-12-08 20:13:48 -0600

    checking env variables for prod

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const http = require("http");
 const { Server } = require("socket.io");
 
@@ -14,7 +15,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: [process.env.CLIENT_URL_PROD],
     methods: ["GET", "POST"],
     credentials: true,
     allowedHeaders: ["Content-Type"],
@@ -23,7 +24,7 @@ const io = new Server(server, {
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // exact origin
+    origin: process.env.CLIENT_URL_PROD,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
