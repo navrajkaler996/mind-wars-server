ace419e20bfe9dcc7084e4023866ca7dec6b405c
Navraj singh kaler <navrajkaler996@gmail.com>
2025-12-30 22:09:16 -0600

    changes

--- src/data.js
+++ src/data.js
@@ -1,62 +1,62 @@
 export const quizData = [
-  // {
-  //   question: "When did World War 2 begin?",
-  //   options: ["1914", "1939", "1945", "1929"],
-  //   answer: "1939",
-  // },
-  // {
-  //   question: "Which country was invaded by Germany to start World War 2?",
-  //   options: ["France", "Poland", "Belgium", "Soviet Union"],
-  //   answer: "Poland",
-  // },
-  // {
-  //   question:
-  //     "Who was the Prime Minister of the United Kingdom during most of World War 2?",
-  //   options: [
-  //     "Winston Churchill",
-  //     "Neville Chamberlain",
-  //     "Clement Attlee",
-  //     "Margaret Thatcher",
-  //   ],
-  //   answer: "Winston Churchill",
-  // },
-  // {
-  //   question: "What was the name of the German air force during World War 2?",
-  //   options: ["Wehrmacht", "Luftwaffe", "SS", "Gestapo"],
-  //   answer: "Luftwaffe",
-  // },
-  // {
-  //   question: "Which event led the United States to enter World War 2?",
-  //   options: [
-  //     "Invasion of Poland",
-  //     "Battle of Britain",
-  //     "Attack on Pearl Harbor",
-  //     "D-Day",
-  //   ],
-  //   answer: "Attack on Pearl Harbor",
-  // },
-  // {
-  //   question:
-  //     "Which battle is considered a turning point on the Eastern Front?",
-  //   options: [
-  //     "Battle of Stalingrad",
-  //     "Battle of Berlin",
-  //     "Battle of Kursk",
-  //     "Battle of Moscow",
-  //   ],
-  //   answer: "Battle of Stalingrad",
-  // },
-  // {
-  //   question:
-  //     "Which two cities were targeted with atomic bombs by the United States?",
-  //   options: [
-  //     "Hiroshima and Nagasaki",
-  //     "Tokyo and Osaka",
-  //     "Kyoto and Hiroshima",
-  //     "Nagasaki and Kyoto",
-  //   ],
-  //   answer: "Hiroshima and Nagasaki",
-  // },
+  {
+    question: "When did World War 2 begin?",
+    options: ["1914", "1939", "1945", "1929"],
+    answer: "1939",
+  },
+  {
+    question: "Which country was invaded by Germany to start World War 2?",
+    options: ["France", "Poland", "Belgium", "Soviet Union"],
+    answer: "Poland",
+  },
+  {
+    question:
+      "Who was the Prime Minister of the United Kingdom during most of World War 2?",
+    options: [
+      "Winston Churchill",
+      "Neville Chamberlain",
+      "Clement Attlee",
+      "Margaret Thatcher",
+    ],
+    answer: "Winston Churchill",
+  },
+  {
+    question: "What was the name of the German air force during World War 2?",
+    options: ["Wehrmacht", "Luftwaffe", "SS", "Gestapo"],
+    answer: "Luftwaffe",
+  },
+  {
+    question: "Which event led the United States to enter World War 2?",
+    options: [
+      "Invasion of Poland",
+      "Battle of Britain",
+      "Attack on Pearl Harbor",
+      "D-Day",
+    ],
+    answer: "Attack on Pearl Harbor",
+  },
+  {
+    question:
+      "Which battle is considered a turning point on the Eastern Front?",
+    options: [
+      "Battle of Stalingrad",
+      "Battle of Berlin",
+      "Battle of Kursk",
+      "Battle of Moscow",
+    ],
+    answer: "Battle of Stalingrad",
+  },
+  {
+    question:
+      "Which two cities were targeted with atomic bombs by the United States?",
+    options: [
+      "Hiroshima and Nagasaki",
+      "Tokyo and Osaka",
+      "Kyoto and Hiroshima",
+      "Nagasaki and Kyoto",
+    ],
+    answer: "Hiroshima and Nagasaki",
+  },
   {
     question: "Who was the leader of Nazi Germany during World War 2?",
     options: [
