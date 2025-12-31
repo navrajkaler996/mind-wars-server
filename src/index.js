9daff4c5a6a74835a130f346f54560256ec4102d
Navraj singh kaler <navrajkaler996@gmail.com>
2025-12-30 22:12:47 -0600

    ready for prod

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: [process.env.CLIENT_URL_DEV],
+    origin: [process.env.CLIENT_URL_PROD],
     methods: ["GET", "POST"],
     credentials: true,
     allowedHeaders: ["Content-Type"],
@@ -30,7 +30,7 @@ const io = new Server(server, {
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL_DEV,
+    origin: process.env.CLIENT_URL_PROD,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
